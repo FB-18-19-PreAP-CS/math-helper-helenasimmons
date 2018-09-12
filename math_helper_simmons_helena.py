4061e4d5dd7cf8709ed081d0597732da8b237355
helenasimmons <helenacctx@gmail.com>
2018-09-12 14:12:19 -0500

    change made

--- math_helper_simmons_helena.py
+++ math_helper_simmons_helena.py
@@ -1,1 +1,12 @@
 
+
+def vertex_form():
+    standard_equation = 'y = a(x-h)^2 + k'
+    a = int(input('a = '))
+    b = int(input('b = '))
+    c = int(input('c = '))
+    x = int(input('x = '))
+    print(a)
+    
+    
+vertex_form()
