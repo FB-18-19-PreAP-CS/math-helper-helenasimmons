d616392200dde6738fdd008b79378073e9fa5f56
helenasimmons <helenacctx@gmail.com>
2018-09-13 14:11:24 -0500

    changes to formulas

--- math_helper_simmons_helena.py
+++ math_helper_simmons_helena.py
@@ -1,6 +1,9 @@
-
+import math 
 
 def vertex_form():
+    '''
+    returns h and k, the coordinates to the vertex.
+    '''
     standard_equation = 'y = a(x-h)^2 + k'
     a = int(input('a = '))
     b = int(input('b = '))
@@ -8,6 +11,19 @@ def vertex_form():
     x = int(input('x = '))
     print(a)
     
+def distance formula():
+    '''
+    returns the distance between two sets of coordinates
+    '''
+    x1 = int(input('x1 = '))
+    y1 = int(input('y1 = '))
+    x2 = int(input('x2 = '))
+    y2 = int(input('y2 = '))
     
-vertex_form()
+    dist = math.sqrt((x1-x2)) #VERY MUCH INCOMPLETE
+    
+    
+if __name__ == "main":
+    import doctest
+    doctest.testmod()
 print()
