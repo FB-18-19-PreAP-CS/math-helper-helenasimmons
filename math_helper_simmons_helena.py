f8391a41eb15b0b828d6251f735136b049a1d821
helenasimmons <helenacctx@gmail.com>
2018-09-17 14:10:53 -0500

    almost done with dist formula

--- math_helper_simmons_helena.py
+++ math_helper_simmons_helena.py
@@ -1,29 +1,42 @@
 import math 
-
+    
 def vertex_form():
-    '''
-    returns h and k, the coordinates to the vertex.
+    '''returns h and k, the coordinates to the vertex.
+
     '''
     standard_equation = 'y = a(x-h)^2 + k'
     a = int(input('a = '))
     b = int(input('b = '))
     c = int(input('c = '))
+    y = int(input('y = '))
     x = int(input('x = '))
+    
     print(a)
+
+#x1 = int(input('x1 = '))
+#y1 = int(input('y1 = '))
+#x2 = int(input('x2 = '))
+#y2 = int(input('y2 = '))
+
+def dist_formula(x1,y1,x2,y2):
+    ''' returns the distance between two sets of coordinates
     
-def distance formula():
+    >>> dist_formula(8,2,3,2)
+    5.0
+    
+    >>> dist_formula(3,7,5,7)
+    2.0
+    
+    >>> dist_formula(-1,-1,-5,-1)
+    4.0
     '''
-    returns the distance between two sets of coordinates
-    '''
-    x1 = int(input('x1 = '))
-    y1 = int(input('y1 = '))
-    x2 = int(input('x2 = '))
-    y2 = int(input('y2 = '))
+    dist = math.sqrt((x1-x2)**2+(y1-y2)**2)
+    print(dist)
     
-    dist = math.sqrt((x1-x2)) #VERY MUCH INCOMPLETE
-    
-    
-if __name__ == "main":
+if __name__ == "__main__":
+    #dist_formula(1,1,1,2)
     import doctest
     doctest.testmod()
+
+
 print()
