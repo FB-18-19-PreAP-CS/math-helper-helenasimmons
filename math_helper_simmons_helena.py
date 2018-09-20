e5a8df7d77d4ceba5f44632dfb33037d5d553b43
helenasimmons <helenacctx@gmail.com>
2018-09-20 14:11:53 -0500

    started point slope and started interface

--- math_helper_simmons_helena.py
+++ math_helper_simmons_helena.py
@@ -1,11 +1,4 @@
 import math 
-    
-#def vertex_form_to_trinomial(a,h,k):
-#    '''returns h and k, the coordinates to the vertex.
-#
-#    '''
-#    standard_equation = 'y = a(x-h)^2 + k'
-
 
 def dist_formula(x1,y1,x2,y2):
     ''' returns the distance between two sets of coordinates
@@ -26,21 +19,21 @@ def dist_formula(x1,y1,x2,y2):
 def pythagorean_theorem(a,b):
     ''' pythagorean theorem. Gives back the value of c. 
     
-    >>> pythagorean_theorem(3,4)  
-    5.0
+    >>> pythagorean_theorem(-3,4)  
+    A side of a triangle shouldn't be negative or zero!
     
-    >>> pythagorean_theorem(7,24) 
-    25.0
+    >>> pythagorean_theorem(0,24) 
+    A side of a triangle shouldn't be negative or zero!
     
     >>> pythagorean_theorem(361,42) 
     363.44
     '''
-    c = math.sqrt(a**2 + b**2)
-    c = round(c,2)
-    print(c)
-    
-    
-    
+    if a <= 0 or b <= 0:
+        print('A side of a triangle shouldn\'t be negative or zero!')
+    elif a != 0 and b != 0:
+        c = math.sqrt(a**2 + b**2)
+        c = round(c,2)
+        print(c)
 
 def multiply_binomials(a1,b1,a2,b2):
     '''multiplies two binomials into a trinomial.
@@ -79,14 +72,54 @@ def slope(x1,y1,x2,y2):
     if run == 0:
         m = "undefined"
         print(m)
-    if rise == 0:
+    elif rise == 0:
         m = 0
         print(m)
-    else:
+    elif run != 0 and rise != 0:
         m = (f"{rise} / {run}")
         print(m)
+        
+def point_slope(x1,y1,m):
+    '''uses point slope form
+
+    >>> point_slope(-4,7, 1/4)
+    y = 1/4x + 8
+    '''
+    s = m * x1
+    b = (-y1) + s
+    o = (f'y = {m}x + {b}')
+    print(o)
+    
+        
+def main():
+    print('Hello there!')
+    print('What do you want to do?')
+    print()
+    print('1. Distance Formula')
+    print('2. Pythagorean Theorem')
+    print('3. Multiply binomials in the form of (ax + b)(cx + d)')
+    print('4. Find the slope between two points.')
+    print('5. Point Slope Form')
+    print()
+    n = input('> ')
+    if n == '1':
+        x1 = int(input('x1: '))
+        y1 = int(input('y1: '))
+        x2 = int(input('x2: '))
+        y2 = int(input('y2: '))
+        dist_formula(x1,y1,x2,y2)
+    if n == '2':
+        a = int(input('a: '))
+        b = int(input('b: '))
+        pythagorean_theorem(a,b)
+    if n == '3':
+        multiply_binomials()
+    if n == '4':
+        slope()        
+
 
 if __name__ == "__main__":
+    main()
     import doctest
     doctest.testmod()
 
